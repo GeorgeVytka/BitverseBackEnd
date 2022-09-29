d2d00ea1527c6b06facde12f6ba33d0f5aef0957
George <finblack20@gmail.com>
2022-09-28 23:28:45 -0400

    fixed mongodb url

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ import articles from "./routes/articles.js";
 import user from "./routes/user.js";
 import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
-const tempUrl =
-  "mongodb+srv://colt:murrowstudent22@cluster0.gtiaiqr.mongodb.net/?retryWrites=true&w=majority";
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+dotenv.config();
 app.use("/api/v1", articles);
 app.use("/api/v1/editor", user);
 const PORT = process.env.PORT || 5005;
 const url = "http://localhost:" + PORT;
 
 mongoose
-  .connect(tempUrl, {
+  .connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
